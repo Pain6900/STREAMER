c88fdc2ddc27e902b50dc67525757e2d150df4df
PAIN <97961226+Pain6900@users.noreply.github.com>
2022-07-15 15:51:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,34 @@ let Discord = require("discord.js-selfbot");
 let client = new Discord.Client();
 
 
-client.login("")
+client.login("ENTER TOKEN HERE")
+//Dont put in secrets - env not supported
 
-client.on("ready",()=>{
 
-  client.user.setActivity("",{
-    type : ""
+client.on("ready", () => {
+
+  client.user.setActivity("ENTER STATUS HERE", {
+    type: "PLAYING/LISTENING/STREAMING/WATCHING"
   })
-  console.log("")
-  
+  console.log(" ______________________________")
+  console.log("| ✅ OPERATION SUCCESSFUL      |")
+  console.log("| ✅ LOGGED IN CLIENT IS ONLINE|")
+  console.log("| ✅ STATUS SET SUCCESSFULLY   |")
+  console.log(" ______________________________")
 })
+
+client.on("message", message => {
+
+  if (message.content.startsWith("ping"))
+      message.channel.send(`Pong🏓`);
+});
+
 
 
 //keep alive
 const express = require("express")
 const app = express()
-
-const server = app.listen(3000, () => {
-    console.log("")
+const server = app.listen(1000, () => {
+  console.log("")
 })
-
-
 app.get("/", (req, res) => res.send(""))
