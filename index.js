f205b23083e2a6972bee45d9bb707b142e6dbd2f
PAIN <97961226+Pain6900@users.noreply.github.com>
2022-07-15 16:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,54 @@
 let Discord = require("discord.js-selfbot");
 let client = new Discord.Client();
 
+let type = "STREAMING"
+let status = ""
+let link = ""
+let token = ""
 
-client.login("ENTER TOKEN HERE")
 //Dont put in secrets - env not supported
 
 
-client.on("ready", () => {
-
-  client.user.setActivity("ENTER STATUS HERE", {
-    type: "PLAYING/LISTENING/STREAMING/WATCHING"
-  })
-  console.log(" ______________________________")
-  console.log("| ✅ OPERATION SUCCESSFUL      |")
-  console.log("| ✅ LOGGED IN CLIENT IS ONLINE|")
-  console.log("| ✅ STATUS SET SUCCESSFULLY   |")
-  console.log(" ______________________________")
-})
+client.login(token)
 
 client.on("message", message => {
 
-  if (message.content.startsWith("ping"))
-      message.channel.send(`Pong🏓`);
+  if (message.content.startsWith("ping") && message.author.id == client.user)
+    message.channel.send(`Pong🏓 | Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 });
+
+
+
+
+client.on("ready", async () => {
+
+  await client.user.setActivity(status, {
+    type: type,
+    url: link
+  })
+  console.log("logged in as :" + client.user)
+  console.log("Status : " + status)
+  console.log("Type : " + type)
+  console.log("URL : " + link)
+
+  console.log(" _______________________________")
+  console.log("  ")
+  console.log("| ✅ OPERATION SUCCESSFUL       |")
+  console.log("| ✅ LOGGED IN CLIENT IS ONLINE |")
+  console.log("| ✅ STATUS SET SUCCESSFULLY    |")
+  console.log(" _______________________________")
+  console.log("  ")
+  console.log("Made by : ⏤͟͟͞⍟・🜲 PAiN ❯❯❯#4005")
+
+
+})
 
 
 
 //keep alive
 const express = require("express")
 const app = express()
-const server = app.listen(1000, () => {
+const server = app.listen(3000, () => {
   console.log("")
 })
 app.get("/", (req, res) => res.send(""))
